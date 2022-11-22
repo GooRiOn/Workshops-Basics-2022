0cfec0aab3d222cce339af34a437370aa9d46894
Dariusz Pawlukiewicz <dpawlukiewicz@ecovadis.com>
2022-11-22 12:07:31 +0100

    Added example of soft delete

--- Rossman.Console/Program.cs
+++ Rossman.Console/Program.cs
@@ -1,97 +1,48 @@
-﻿// C# 1 Begin/Invoke
+﻿
+using System.Globalization;
 
-// C# 2 Event-Based
+var user = new User();
 
-// C# Task-Based
-
-// async/await 
-
-using System.Runtime.CompilerServices;
-using System.Text;
-using External.Assembly;
-var service = new UsersService();
-
-//var task2 = new Task2();
-
-//await task2;
-
-var user1 =  service.GetByIdAsync(Guid.NewGuid());
-var user2 =  await service.GetByIdAsync(Guid.NewGuid());
-
-Console.ReadLine();
-var ids = new[] {Guid.NewGuid(), Guid.NewGuid(),};
-//var users = new List<User>();
-
-//var tasks = ids
-  //  .Select(id => service.GetByIdAsync(id));
-
-//var users = await Task.WhenAll(tasks);
-
+var number = ((IConvertible) "aa").ToInt32(null);
 Console.WriteLine();
 
-//First - FirstOrDefault
-//Single - SingleOrDefault
-
-public class UsersService
+public interface ISoftDeletable
 {
-    private readonly DatabaseConnector _connector = new();
-
-    public Task<User> GetByIdAsync(Guid id)
-        => _connector.GetByIdAsync(id, 10_000);
+    DateTimeOffset? DeletionDate { get; set; }
+    bool  IsDeleted => DeletionDate.HasValue;
+    void SoftDelete();
 }
 
-public class DatabaseConnector
+
+public class User : ISoftDeletable// SOFT DELETE
 {
-    public User GetById(Guid id)
+    public Guid Id { get; set; }
+    DateTimeOffset? ISoftDeletable.DeletionDate { get; set; }
+    
+    void ISoftDeletable.SoftDelete()
     {
-        Console.WriteLine("Connecting to db...");
-        Thread.Sleep(10_000);
-        Console.WriteLine("Connected to db!");
-
-        return new User {Id = id, FullName = "Joe Doe", Age = 18};
-    }
-
-    public async Task<User> GetByIdAsync(Guid id, int delay)
-    {
-        Console.WriteLine("Connecting to db...");
-        await Task.Delay(delay); // throw new NotImplementedException();
-        Console.WriteLine("Connected to db!");
-
-        return new User {Id = id, FullName = "Joe Doe", Age = 18};
-    }
-
-    public async void Test() // fire and forget
-    {
-        Console.WriteLine("Test");
-    }
-
-    //C# 4 
-    public void Test2()
-    {
-        var await = 2;
+        ((ISoftDeletable)this).DeletionDate = DateTimeOffset.Now;
     }
 }
 
-//
-
-public class Task2
+public class Product // HARD
 {
-    public TaskAwaiter2 GetAwaiter()
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+}
+
+
+class DbConnector<TEntity>
+{
+    public void Delete(TEntity entity)
     {
-        return new TaskAwaiter2();
-    }
+        if (entity is ISoftDeletable softDeletable)
+        {
+            softDeletable.SoftDelete();
+            // DB UPDATE
+            return;
+        }
         
-}
-
-public class TaskAwaiter2 : INotifyCompletion
-{
-    public bool IsCompleted { get; set; } = true;
-    public void OnCompleted(Action continuation)
-    {
-    }
-
-    public void GetResult()
-    {
-        Console.WriteLine("WOW!");
+        // DB DELETE
     }
 }
